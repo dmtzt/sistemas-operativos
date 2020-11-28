60dd9cdd1d325a6f1cf9986724f5bd7302a0d725
David Martínez <david.mtztristan@gmail.com>
2020-11-27 20:10:00 -0600

    Refactorización de método de parsing de argumentos
    Se refactorizó el método de parsing de argumentos para que soportara diferentes casos de uso

--- page-replacement.cpp
+++ page-replacement.cpp
@@ -18,7 +18,9 @@
 
 using namespace std;
 
+bool parseArg(string &arg, string request, int &pos);
 bool loadProcess(string request);
+void testString(string s);
 
 int main(void)
 {
@@ -94,37 +96,106 @@ int main(void)
 
 bool loadProcess(string request)
 {
+    int pos = 1;
     // Número de bytes y número de proceso
-    string bytes, process;
-    // Control de posición para extracción de datos de solicitud
-    int pos = 2;
-    
-    // Extraer número de bytes
-    bytes = request.substr(pos, request.find(' ', pos) - pos);
-    // Actualizar posición de apoyo para encontrar el siguiente valor
-    pos = request.find(' ', pos) + 1;
-    // Extraer número de proceso
-    process = request.substr(pos, request.find(' ', pos) - pos); 
-    
-    // Verificar que los elementos en bytes y proceso sean digitos
-    for (char digit : bytes)
+    string bytes = "";
+    string process = "";
+
+    if (!parseArg(bytes, request, pos))
     {
-        if (!isdigit(digit))
-            return false;
+        cout << "El numero de bytes no pudo ser extraido" << endl;
+        return false;
+    }
+    else if (!parseArg(process, request, pos))
+    {
+        cout << "El numero de proceso no pudo ser extraido" << endl;
+        return false;
+    }
+    else
+    {
+        int n = stoi(bytes);
+        int p = stoi(process);
+        cout << n << " " << p << endl;
+
+        return true;
+    }
+}
+
+/*
+ * Helper method
+ * Parse a single argument from request, from the first digit found 
+ * until a space to the right is found
+ * 
+ * The position variable is updated by reference for future extractions in same request
+ *
+ * Handles the following cases:
+ * 1. Success: The argument is preceded by or followed by several spaces
+ * 2. Success: The argument is followed by end of request (null char)
+ * 3. Error: The argument is composed by non-digit characters
+ * 4. Error: The argument is missing
+*/
+bool parseArg(string &arg, string request, int &pos) 
+{
+    // Validate function args
+    if (pos >= request.length())
+    {
+        cout << "Invalid start position for argument parsing" << endl;
+        return false;
     }
 
-    for (char digit : process)
-    {
-        if (!isdigit(digit))
-            return false;
-    }
-    
-    // Número de bytes y número de proceso como enteros
-    int n = stoi(bytes);
-    int p = stoi(process);;
-    
-    // Imprimir número de bytes y número de proceso
-    cout << n << " " << p << endl;
+    // Control state to know if prev char was a digit
+    bool prevWasDigit = false;
 
-    return true;
+    // Iterate over the request and append digits to request arg as soon as 
+    // one is found, until they are over and one adjacent space is found
+    while (pos < request.length())
+    {
+        // Find the next digit until an adjacent space is found
+        if (prevWasDigit == false || request[pos] != ' ')
+        {
+            // If current char is a digit
+            if (isdigit(request[pos]))
+            {
+                // Change control state to true
+                prevWasDigit = true;    
+                // Append digit
+                arg += request[pos];
+            }
+            // If a space is found
+            else if (request[pos] == ' ')
+                // Change control state to false
+                prevWasDigit = false;
+            // Error: a char not digit or space was found
+            else
+            {
+                cout << "A non-numeric character was found in argument" << endl;
+                return false;
+            }
+
+            // Update position in request
+            pos++;
+        }
+        // Success: A space after a digit has been found
+        else
+        {
+            //testString(arg);
+            return true;
+        }        
+    }
+
+    // The last digit was the last char of the request, no subsequent spaces
+    if (pos == request.length() && prevWasDigit)
+    {
+        //testString(arg);
+        return true;
+    }
+
+    // Search excedes request length and no argument was found
+    cout << "No argument was found" << endl;
+    return false;
+}
+
+void testString(string s) 
+{
+    cout << "|" << s << "|" << endl;
 }
